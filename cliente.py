c3a88df19cf041ac3cbccdc06627924c90fd2d76
CrisTowi <christian_consuelo@outlook.com>
2014-04-20 01:38:57 -0500

    Juego casi terminado, falta obtener canador

--- cliente.py
+++ cliente.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import threading  
 import socket
 import pickle
-from time import time
+from time import time, sleep
+ 
+words_found = []
+nombre_jugador = ''
+
+def contar_tiempo(start_time):
+  print('Entramos a contar el tiempo')
+  sleep(20)
+  global s
+  print(words_found)
+  s.send(pickle.dumps(['juego_terminado', nombre_jugador ,words_found]))
+
 def print_puzzle(sopa, width, height, word_bank):
 	for word in word_bank:
 		print word.center(width * 2)
@@ -15,14 +26,16 @@ def print_puzzle(sopa, width, height, word_bank):
 		print
      
 
-def juego(puzzle, word_bank, word_coords):
+def juego(puzzle, word_coords, word_bank):
+
   start_time = time()
-  print start_time
+  t = threading.Thread(target=contar_tiempo, args=(start_time,  ))
+  t.start()
   # Start the timer
-  words_found = []
+  global words_found
   while len(words_found) < len(word_bank):
       print_puzzle(puzzle, 15, 15, word_bank)
-      word = raw_input("Qué palabra encontraste? ").upper()
+      word = raw_input("Qué palabra encontraste? ")
       print word
       if word not in word_bank:
 				# not in the word bank
@@ -34,13 +47,15 @@ def juego(puzzle, word_bank, word_coords):
       try:
           x, y = coords.split(",")
           x, y = int(x.strip()) - 1, int(y.strip()) - 1
+          x = int(x)
+          y = int(y)
       except ValueError, IndexError:
           # invalid format
           print "Las coordenadas deben estár separadas por comas"
           continue
       else:
           if (x, y) == word_coords[word.upper()]:
-              words_found.append(word)
+              words_found.append([word, time() - start_time])
               print '"%s" encontrado!. %s faltantes.' % (
                   word, len(word_bank) - len(words_found)
               )
@@ -51,22 +66,25 @@ def juego(puzzle, word_bank, word_coords):
       time() - start_time)
   # Show the time elapsed
 
-
 s = socket.socket()
-s.connect(("localhost", 8001))
+s.connect(("localhost", 8000))
 
-buffzise = 10000
+buffzise = 50000
 
 while(True):
-	if(raw_input('Quieres recibir? h/a ') == 'h'):
-		s.send('hola')
-		recv = s.recv(buffzise)
-		mensaje = pickle.loads(recv)
-		juego(mensaje[0], mensaje[1], mensaje[2])
-	else:
-		s.send('adios')
-		mensaje = s.recv(buffzise)
-		sopa = pickle.loads(mensaje)
-		print_puzzle(sopa,15,15)
+  if(raw_input('Quieres empezar a jugar? y/n ') == 'y'):
+    global nombre_jugador
+    nombre_jugador = raw_input('Cual es tu nombre de jugador?')
+    s.send(pickle.dumps(['hola']))
+    print('Esperando a los demás jugadores...')
+    recv = s.recv(buffzise)
+    mensaje = pickle.loads(recv)
+    print mensaje[1]
+    juego(mensaje[0], mensaje[1], mensaje[2])
+  else:
+    s.send(pickle.dumps(['adios']))
+    mensaje = s.recv(buffzise)
+    sopa = pickle.loads(mensaje)
+    print_puzzle(sopa,15,15)
 
 s.close()
--- servidor.py
+++ servidor.py
@@ -6,23 +6,31 @@ from wordsearch import Sopa
 import pickle
 
 sopa = Sopa(15,15)
+num_jugadores = 1
+resultados_jugadores = []
 
 def enviar_info(sc, address, sopa):
 
 	seguir =  True
 	while (seguir):
-		peticion = sc.recv(1000)
-		if ("adios"!=peticion):
+		peticion = pickle.loads(sc.recv(50000))
+
+		if ('hola'==peticion[0]):
 			print str(address)+ " envia hola: contesto"
 			sc.send(pickle.dumps([sopa.sopa, sopa.word_coords, sopa.word_bank]))
              
          # Contestacion y cierre a "adios"
-		if ("adios"==peticion):
+		if ('adios'==peticion[0]):
 			print str(address)+ " envia adios: contesto y desconecto"
 			sc.send("pues adios")
 			sc.close()
 			print "desconectado "+str(address)
 			seguir = False
+
+		if ('juego_terminado'==peticion[0]):
+			resultados_jugadores.append([peticion[1], peticion[2]])
+			seguir = False
+
 
 def correr_hilos(arreglo_sockets):
 	for i in range(len(arreglo_sockets)):
@@ -30,7 +38,7 @@ def correr_hilos(arreglo_sockets):
 		arreglo_sockets[i].start()
 
 s = socket.socket()
-s.bind(("localhost", 8001))
+s.bind(("localhost", 8000))
 s.listen(5)
 
 arreglo_sockets = []
@@ -44,7 +52,7 @@ while (True):
 	arreglo_sockets.append(t)
 
 	contador = contador + 1
-	if(contador == 2):
+	if(contador == num_jugadores):
 		correr_hilos(arreglo_sockets)
 		contador = 0
 	print(contador)
